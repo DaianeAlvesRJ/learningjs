85808a6bb56eedf03b50c42f5f4bd45fe8125adc
Daiane <dsalves@id.uff.br>
2017-06-27 10:54:48 -0300

    Correção da identação e remoção dos excessos de parênteses e ponto e vírgulas

--- desafio.js
+++ desafio.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var nota1 = 8.0;
-var nota2 = 5.0;
-var nota3 = 6.0;
-var media = ((nota1*3) + (nota2*4) + (nota3*5))/12;
-if (media>=6)
-    {
-        console.log("Aluno APROVADO!")
-    }
-    else {
-        console.log("Aluno REPROVADO!")
-    }
+var nota1 = 8.0
+var nota2 = 5.0
+var nota3 = 6.0
+var media = (nota1 * 3 + nota2 * 4 + nota3 *5 )/12
+if (media>=6){
+    console.log("Aluno APROVADO!")
+} else {
+    console.log("Aluno REPROVADO!")
+}
 
