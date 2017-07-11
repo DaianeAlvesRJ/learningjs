9f6cd70a8d3ec021a9e4a3d89bcdaa927d6ba852
jucieny <jucieny.santos@f1solucoes.com.br>
2017-07-11 11:19:30 -0300

    Adicionado exercicio para adicionar endereços

---
+++ enderecos.js
@@ -0,0 +1,11 @@
+
+var usuario = {
+    nome: 'Fulano Beltrano Filho',
+    idade: 34,
+    enderecos: [
+        { id: 1, rua: 'Lindolfo Color', numero: 104, bairro: 'Centro', cidade: 'São Leopoldo', estado: 'RS', cep: '93.000.000' }
+    ]
+}
+
+// criar funções para alterar nome e idade do usuario
+// criar função que adicione mais endereços, o endereco atual não deve ser apagado
