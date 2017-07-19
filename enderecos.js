59586c86c7d503116f74836f606903fe74b30156
jucieny <jucieny.santos@f1solucoes.com.br>
2017-07-19 09:41:23 -0300

    adicionado os enderecos

--- enderecos.js
+++ enderecos.js
@@ -3,9 +3,12 @@ var usuario = {
     nome: 'Fulano Beltrano Filho',
     idade: 34,
     enderecos: [
-        { id: 1, rua: 'Lindolfo Color', numero: 104, bairro: 'Centro', cidade: 'São Leopoldo', estado: 'RS', cep: '93.000.000' }
+        { id: 1, rua: 'Lindolfo Color', numero: 104, bairro: 'Centro', cidade: 'São Leopoldo', estado: 'RS', cep: '93.000.000' },
     ]
 }
+
+usuario.enderecos.push({nome: "nome"})
+console.log(usuario)
 
 // criar funções para alterar nome e idade do usuario
 // criar função que adicione mais endereços, o endereco atual não deve ser apagado
